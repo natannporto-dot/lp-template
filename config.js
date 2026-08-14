2e173684697e606723de4f0aad47340021141092
Natan Porto Spricigo <natannporto@gmail.com>
2026-08-13 21:39:44 -0300

    Adiciona configuração base do cliente

---
+++ config.js
@@ -0,0 +1,9 @@
+const clientConfig = {
+  companyName: "Vinícius Marcelino",
+  companyDescription: "Diagnóstico, aquisição e conversão para SaaS B2B.",
+  whatsapp: "",
+  email: "",
+  logo: "./assets/logo.svg",
+  favicon: "./assets/favicon.svg",
+  primaryColor: "#f7f7f3",
+};
